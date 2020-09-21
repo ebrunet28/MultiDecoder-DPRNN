a25049eca32c83754a7b81c2285ee1a1b69d7506
Joseph Zhu <junzhez2@ifp-10.ifp.uiuc.edu>
2020-09-21 04:52:05 -0500

    runs

--- configs/config4.py
+++ configs/config4.py
@@ -31,7 +31,7 @@ checkpoint = 1
 continue_from = os.path.join(save_folder, "config4.pth") # if not exist, randomly initialize
 model_path = config + "_best.pth" # best model save path
 print_freq = 10
-comment = ' multiple decoder with the same TCN. Random Initialization'
+comment = ' weighted chunk resampling'
 log_dir = os.path.join(root, 'runs', time.strftime("%Y%m%d-%H%M%S") + config + comment)
 use_onoff = True # hungarian model if on, DPRNN if off. useless if multidecoder=True
 multiloss = True
